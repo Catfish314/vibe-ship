fccab29cfcd76ad11b6c4db35ddeac9402590215
Sviatoslav <sviatoslavpianist@gmail.com>
2026-04-18 21:34:43 +0300

    Add new features

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+//Треба додати щось корисне
 createRoot(document.getElementById("root")!).render(<App />);
